31017c30b5199b84268554f2c79564dfdc51c4a8
Maruf Mobin <mobinh264@gmail.com>
2026-06-21 23:15:15 +0600

    Add Navigator Setup

---
+++ lib/app/zulzana_app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import '../features/auth/presentation/screens/splash_screen.dart';
+import 'routes.dart';
+
+class ZulzanaApp extends StatelessWidget {
+  const ZulzanaApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Zulzana App',
+      initialRoute: SplashScreen.name,
+      onGenerateRoute: AppRoutes.onGenerateRoute,
+    );
+  }
+}
